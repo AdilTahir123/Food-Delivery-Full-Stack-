d1572907a244e2c010bd86f9f19e6f5795dada20
AdilTahir123 <adiltahirmaqbool@gmail.com>
2026-09-05 14:33:20 +0500

    Update Css

--- Food-Del/src/pages/PlaceOrder/PlaceOrder.jsx
+++ Food-Del/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,11 +1,22 @@
 import React, { useState, useContext, useEffect } from "react";
+
 import "./PlaceOrder.css";
+
 import { StoreContext } from "../../components/context/StoreContext";
+
 import axios from "axios";
+
 import { useNavigate } from "react-router-dom";
+
 const PlaceOrder = () => {
-  const { getTotalCartAmount, token, food_list, cartItems, url } =
-    useContext(StoreContext);
+
+  const {
+    getTotalCartAmount,
+    token,
+    food_list,
+    cartItems,
+    url
+  } = useContext(StoreContext);
 
   const [data, setData] = useState({
     firstname: "",
@@ -21,7 +32,11 @@ const PlaceOrder = () => {
 
   const onChangeHandler = (event) => {
     const { name, value } = event.target;
-    setData((prevData) => ({ ...prevData, [name]: value }));
+
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
   const placeOrder = async (event) => {
@@ -30,52 +45,82 @@ const PlaceOrder = () => {
     let orderItems = [];
 
     food_list.forEach((item) => {
+
       if (cartItems[item._id] > 0) {
+
         let itemInfo = {
           ...item,
           quantity: cartItems[item._id],
         };
+
         orderItems.push(itemInfo);
       }
+
     });
 
     let orderData = {
       address: data,
       items: orderItems,
-      amount: getTotalCartAmount() + (getTotalCartAmount() > 0 ? 2 : 0),
+      amount:
+        getTotalCartAmount() +
+        (getTotalCartAmount() > 0 ? 2 : 0),
     };
 
     try {
-      let response = await axios.post(`${url}/api/order/place`, orderData, {
-        headers: { token },
-      });
+
+      let response = await axios.post(
+        `${url}/api/order/place`,
+        orderData,
+        {
+          headers: { token },
+        }
+      );
 
       if (response.data.success) {
+
         const { session_url } = response.data;
+
         window.location.replace(session_url);
+
       } else {
+
         alert("Error");
+
       }
+
     } catch (error) {
+
       console.log(error);
       alert("Something went wrong");
+
     }
   };
-  const navigate=useNavigate();
-  useEffect(()=>{
-    if(!token){
-      navigate('/cart');
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+
+    if (!token || getTotalCartAmount() === 0) {
+      navigate("/cart");
     }
-    else if(getTotalCartAmount()===0){
-      navigate('/cart');
-    }
-  },[token])
+
+  }, [token]);
+
   return (
-    <form onSubmit={placeOrder} className="place-order">
+
+    <form
+      onSubmit={placeOrder}
+      className="place-order"
+    >
+
       <div className="place-order-left">
-        <p className="title">Delivery Information</p>
+
+        <p className="title">
+          Delivery Information
+        </p>
 
         <div className="multi-fields">
+
           <input
             name="firstname"
             onChange={onChangeHandler}
@@ -84,6 +129,7 @@ const PlaceOrder = () => {
             placeholder="First Name"
             required
           />
+
           <input
             name="lastname"
             onChange={onChangeHandler}
@@ -91,6 +137,7 @@ const PlaceOrder = () => {
             type="text"
             placeholder="Last Name"
           />
+
         </div>
 
         <input
@@ -101,6 +148,7 @@ const PlaceOrder = () => {
           placeholder="Email Address"
           required
         />
+
         <input
           name="street"
           value={data.street}
@@ -111,6 +159,7 @@ const PlaceOrder = () => {
         />
 
         <div className="multi-fields">
+
           <input
             name="city"
             value={data.city}
@@ -118,6 +167,7 @@ const PlaceOrder = () => {
             type="text"
             placeholder="City"
           />
+
           <input
             name="state"
             value={data.state}
@@ -125,9 +175,11 @@ const PlaceOrder = () => {
             type="text"
             placeholder="State"
           />
+
         </div>
 
         <div className="multi-fields">
+
           <input
             name="zipcode"
             value={data.zipcode}
@@ -135,6 +187,7 @@ const PlaceOrder = () => {
             type="text"
             placeholder="Zip Code"
           />
+
           <input
             name="country"
             value={data.country}
@@ -142,6 +195,7 @@ const PlaceOrder = () => {
             type="text"
             placeholder="Country"
           />
+
         </div>
 
         <input
@@ -151,10 +205,13 @@ const PlaceOrder = () => {
           type="text"
           placeholder="Phone Number"
         />
+
       </div>
 
       <div className="place-order-right">
+
         <div className="cart-total">
+
           <h2>Cart Totals</h2>
 
           <div className="cart-total-details">
@@ -166,19 +223,30 @@ const PlaceOrder = () => {
 
           <div className="cart-total-details">
             <p>Delivery Fee</p>
-            <p>${getTotalCartAmount() > 0 ? 2 : 0}</p>
+            <p>
+              ${getTotalCartAmount() > 0 ? 2 : 0}
+            </p>
           </div>
 
           <hr />
 
           <div className="cart-total-details">
             <p>Total</p>
-            <p>${getTotalCartAmount() + (getTotalCartAmount() > 0 ? 2 : 0)}</p>
+            <p>
+              $
+              {getTotalCartAmount() +
+                (getTotalCartAmount() > 0 ? 2 : 0)}
+            </p>
           </div>
 
-          <button type="submit">Proceed to Payment</button>
+          <button type="submit">
+            Proceed to Payment
+          </button>
+
         </div>
+
       </div>
+
     </form>
   );
 };
